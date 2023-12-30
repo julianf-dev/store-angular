6b36424e3b9f9b5a742cbc99a46efdadf6ca09f8
Julian F <juliandavidbf@gmail.com>
2023-12-30 01:39:24 -0500

    avanec ipes

---
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDistanceToNow } from 'date-fns';
+import { es } from 'date-fns/locale';
+
+@Pipe({
+  name: 'timeAgo',
+  standalone: true
+})
+export class TimeAgoPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return formatDistanceToNow(
+      new Date('2028-08-20T12:00:00'), {
+      addSuffix: true,
+      locale: es
+      })
+  }
+
+}
